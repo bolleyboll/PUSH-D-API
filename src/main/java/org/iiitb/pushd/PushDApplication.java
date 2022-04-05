bc9f4089feb9d8571894326599ef735a82e83a3d
Aman Gupta <aman.iv0012@gmail.com>
2022-04-05 21:38:57 +0530

    Email Started

--- src/main/java/org/iiitb/pushd/PushDApplication.java
+++ src/main/java/org/iiitb/pushd/PushDApplication.java
@@ -1,11 +1,36 @@
 package org.iiitb.pushd;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 @SpringBootApplication
 public class PushDApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PushDApplication.class, args);
 	}
+
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+
+		mailSender.setUsername("aman.iv0012@gmail.com");
+		mailSender.setPassword("password");
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
+	}
+
 }
---
+++ src/main/java/org/iiitb/pushd/mail/MailerServiceImpl.java
@@ -0,0 +1,9 @@
+package org.iiitb.pushd.mail;
+
+public class MailerServiceImpl implements MailerService {
+
+	@Override
+	public void sendSimpleMessage(String to, String subject, String text) {
+
+	}
+}
