10550055c26de1ca5fcd495b1d5cc9022ba35e19
shuddd <shuddd@gmail.com>
2022-05-09 21:46:55 +0530

    Update ChatController.java

--- src/main/java/org/iiitb/pushd/controller/ChatController.java
+++ src/main/java/org/iiitb/pushd/controller/ChatController.java
@@ -3,6 +3,7 @@ package org.iiitb.pushd.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.iiitb.pushd.models.Chat;
 import org.iiitb.pushd.services.ChatService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("chat")
 public class ChatController {
